663dc56f654c3cd53cece41210fd755a0e71e55f
monkeyWangs <2424880409@qq.com>
2017-09-07 17:26:04 +0800

    add rollup to pack index.js

--- example/views/async.vue
+++ example/views/async.vue
@@ -1,4 +1,4 @@
-<<template>
+<template>
   <div>
       {{pageName === 'loading' ? pageName : 'this is '+pageName+' page'}}
   </div>
---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from 'rollup-plugin-commonjs'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import json from 'rollup-plugin-json'
+import buble from 'rollup-plugin-buble'
+
+const pkg = require('./package.json')
+
+export default {
+  entry: './src/index.js',
+  targets: [
+    { dest: pkg.main, format: 'umd', moduleName: 'VueMetaInfo' }
+  ],
+  plugins: [
+    json(),
+    nodeResolve({ jsnext: true }),
+    commonjs(),
+    buble()
+  ],
+  banner: `
+    /**
+     * vue-meta-info v${pkg.version}
+     * (c) ${new Date().getFullYear()} monkeyWang
+     * @license MIT
+     */
+  `.replace(/ {4}/gm, '').trim()
+}
